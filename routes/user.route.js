80e3d5071da3db00442c75fae46fde051a4a9002
Emi Bellissima <emibellissima@gmail.com>
2019-09-20 19:47:40 +0200

    Added listUsers

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,7 @@ const UserController = require('../controllers/user.controller.js');
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/my-profile', auth, UserController.profile);
+router.get('/', auth, UserController.listUsers);
 // router.put('/my-profile/:id', auth, UserController.updateProfile);
 
 module.exports = router;
