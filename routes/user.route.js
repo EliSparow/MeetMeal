772254d5cf7ce0cde45a8fe3473ea8f120f55b46
fabolododo <fabolododo@hotmail.fr>
2019-09-20 12:05:52 +0200

    uncomment login route

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const UserController = require('../controllers/user.controller.js');
 
 router.post('/register', UserController.register);
-// router.post('/login', UserController.login);
+router.post('/login', UserController.login);
 
 module.exports = router;
