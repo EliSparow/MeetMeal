04909cc4885d02d2601059c2e0561b46f68260df
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-20 17:09:04 +0200

    Modified user.route.js : adding the my-profile route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const UserController = require('../controllers/user.controller.js');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
+router.get('/my-profile', auth, UserController.profile);
+// router.put('/my-profile/:id', auth, UserController.updateProfile);
 
 module.exports = router;
