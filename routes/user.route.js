6e979a4967bff0af0a1a8395fd435e6b502015fb
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-23 13:05:57 +0200

    Modified user.route.js : uncommenting the updateProfile route

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,6 +8,6 @@ router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/my-profile', auth, UserController.profile);
 router.get('/', auth, UserController.listUsers);
-// router.put('/my-profile/:id', auth, UserController.updateProfile);
+router.put('/:id', auth, UserController.updateProfile);
 router.delete('/:id', auth, UserController.deleteUser);
 module.exports = router;
