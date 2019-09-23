3b95370fd83a0a487cfb0325bbfb8722a6c0de14
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-23 14:43:21 +0200

    Modified user.controller.js : removing some content

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -129,7 +129,7 @@ exports.login = async function(req, res) {
 exports.profile = async function(req, res) {
     try {
         const user = await User.findById(req.user.id).select('-password');
-        res.json(user);user
+        res.json(user);
     } catch (err) {
         console.error(err);
         res.status(500).send('Erreur serveur');
