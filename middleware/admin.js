c03c2d8222e69b4f2597f5c3162531bab3d43275
maxim3andr3 <maxim3andr3@gmail.com>
2019-09-24 11:44:59 +0200

    Added admin.js : middleware to check if the user is an admin

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,24 +1,17 @@
-// const jwt = require('jsonwebtoken');
-
-const User = require('../models/user.model');
+const User = require('../models/user.model.js');
 
 /**
-  * @param {*} req
-  * @param {*} res
-  * @param {*} next
-  * @returns
-  */
-
+ * Module to check if the user is an admin or not
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 module.exports = async function(req, res, next) {
-    try{
-        let userAdmin = await User.findById(req.user.id).select('admin');
-        
-        if(userAdmin !== true){
-            res.status(401).json({ msg: 'Permission denied' })
-        }
+    let user = await User.findById(req.user.id);
+    if(user.admin) {
         next();
-
-    } catch(err) {
-        res.status(500).json({ msg: 'Server Error' })
+    } else {
+        res.status(401).send("Acces non autorise");
     }
 }
