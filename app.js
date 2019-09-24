f75bad36a4594afe001cea0e3d57463b4b91bb56
fabolododo <fabolododo@hotmail.fr>
2019-09-24 19:16:27 +0200

    added app.use search for search route

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express();
 const connectDb = require('./config/connectDb');
 const user = require("./routes/user.route");
 
+const search = require("./routes/search.route");
+
 //port requirement 
 const port = process.env.PORT;
 
@@ -45,6 +47,8 @@ app.get('/test', (req,res) => {
 })
 
 app.use("/users", user);
+
+app.use("/search", search);
 
 app.use(express.json({extended: false}));
 app.listen(port, () => console.log(`server run on port ${port}`));
