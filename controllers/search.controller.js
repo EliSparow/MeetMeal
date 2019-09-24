969f1f1a0c12ac0f3a2029767aa8e138ba402b6b
fabolododo <fabolododo@hotmail.fr>
2019-09-24 19:18:03 +0200

    added search controller with search user functions and search event function

---
+++ controllers/search.controller.js
@@ -0,0 +1,90 @@
+const User = require('../models/user.model');
+const Event = require('../models/event.model');
+
+
+/**
+  * This function search an user
+  * 
+  * @param {*} req
+  * @param {*} res
+  * @returns
+ */
+
+exports.user = async function(req, res) {
+    const { searchUser } = req.body;
+    console.log(searchUser);
+    
+    if(!searchUser) {
+        return res.status(400).json({
+            msg: "Entrez un mot-cle."
+        });
+    }
+
+    try {
+        let result = await User.find({
+            $or: [
+                {
+                    firstname: { $regex: searchUser }
+                }, 
+                {
+                    lastname: { $regex: searchUser.toUpperCase()}
+                }
+            ] , 
+        });
+
+        if(result == "") {
+            return res.status(404).json({
+                msg: "Utilisateur non trouve"
+            });
+        }
+        res.status(200).json({ result })
+    } catch(err){
+        res.status(500).json({
+            msg: "Erreur Serveur"
+        });
+    }
+}
+
+exports.event = async function(req, res) {
+    const { searchEvent } =req.body;
+    console.log(searchEvent);
+    
+    if(!searchEvent) {
+        return res.status(400).json({
+            msg: "Entrez un mot-cle."
+        });
+    }
+
+    try {
+        let result = await Event.find({
+            $or: [
+                {
+                    title: { $regex: searchEvent }
+                },
+                {
+                    typeOfCuisine: { $regex: searchEvent }
+                },
+                {
+                    typeOfTheEvent: { $regex: searchEvent }
+                },
+                {
+                    ingredients: { $regex: searchEvent }
+                },
+                {
+                    city: { $regex: searchEvent }
+                }
+            ] ,
+        });
+
+        if(result == "") {
+            return res.status(404).json({
+                msg: "Event non trouve"
+            });
+        }
+        res.status(200).json({ result })
+    } catch(err){
+        res.status(500).json({
+            msg: "Erreur Serveur"
+        });
+    }
+}
