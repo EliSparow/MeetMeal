66a7ccbec9025090d5e5ea00c67db8f816b80049
ParkLySan <lisa@toulzac.fr>
2019-09-26 12:50:29 +0200

    update doc and commentary

--- routes/event.route.js
+++ routes/event.route.js
@@ -5,15 +5,25 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const EventController = require('../controllers/event.controller.js');
 
+//create event
 router.post('/create', auth, EventController.create);
+//show event by ID
 router.get('/:id', auth, EventController.showEvent);
+//show event
 router.get('/', auth, EventController.listEvents);
+//update event
 router.put('/:id', auth, EventController.updateEvent);
+//validate event
 router.put('/:id/validate', auth, admin, EventController.validEvent);
+//refuse event
 router.put('/:id/refuse', auth, admin, EventController.refuseEvent);
+//add guest
 router.put('/:id/addGuest', auth, EventController.addGuest);
+//remove guest
 router.put('/:id/removeGuest', auth, EventController.removeGuest);
+//accept guest
 router.put('/:event_id/acceptGuest/:acceptedGuest_id', auth, EventController.acceptGuest);
+//refuse guest
 router.put('/:event_id/refuseGuest/:refusedGuest_id', auth, EventController.refuseGuest);
 
 module.exports = router;
