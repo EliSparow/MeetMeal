c8ec5944387637d9c26c1485d967d016d2d17a9b
maxim3andr3 <maxim3andr3@gmail.com>
2019-10-02 14:37:26 +0200

    Modified user.controller.js : adding new annotations to add a documentation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,12 +4,30 @@ const { check } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
 /**
- * This function registered an user if every credentials are valide
- *
- * @param {*} req
- * @param {*} res
- * @returns
- * @access Public
+ * @api {post} /users/register register
+ * @apiName register
+ * @apiGroup users
+ * @apiDescription Cette fonction enregistre un utilisateur si tous les champs sont entres
+ * 
+ * @apiParam {String} firstname Prenom de l'utilisateur
+ * @apiParam {String} lastname Nom de l'utilisateur
+ * @apiParam {Number} age Age de l'utilisateur
+ * @apiParam {String} email Email de l'utilisateur
+ * @apiParam {String} password Mot de passe de l'utilisateur
+ * 
+ * @apiSuccess {object} user Profil utilisateur cree
+ * @apiSuccessExample {json} Success-Response
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "msg" : "Utilisateur enregistre"
+ *      }
+ * 
+ * @apiError ChampsObligatoires Tous les champs sont obligatoires
+ * @apiError AgeInvalide L'age entre n'est pas un nombre
+ * @apiError AgeMinimum L'utilisateur doit avoir 18 ans ou plus pour s'enregistrer
+ * @apiError EmailInvalide L'email entre n'est pas conforme
+ * @apiError PasswordInvalide Le mot de passe doit avoir entre 6 et 20 caracteres
+ * @apiError UtilisateurDejaEnregistre L'utilisateur existe deja
  */
 
 exports.register = async function(req, res) {
@@ -66,12 +84,21 @@ exports.register = async function(req, res) {
 }
 
 /**
- * This functions logged an user if every credentials are valide
- *
- * @param {*} req
- * @param {*} res
- * @returns
- * @access Public
+ * @api {post} /users/login login
+ * @apiName login
+ * @apiGroup users
+ * @apiDescription Cette fonction permet a un utilisateur de se connecter
+ * 
+ * @apiParam {String} email Email de l'utilisateur
+ * @apiParam {String} password Mot de passe de l'utilisateur
+ * 
+ * @apiSuccess {String} token Token de la session defini
+ * @apiSuccess {String} id Id de l'utilisateur
+ * 
+ * @apiError EmailNecessaire Le champs email est vide
+ * @apiError PasswordNecessaire Le champs mot de passe est vide
+ * @apiError UtilisateurInconnu L'email entre n'est pas reconnu
+ * @apiError PasswordInvalide Le mot de passe lie a l'email entre n'est pas valide
  */
 
 exports.login = async function(req, res) {
@@ -131,12 +158,21 @@ exports.login = async function(req, res) {
 }
 
 /**
- * Get profile of the user connected
- *
- * @param {*} req
- * @param {*} res
- * @returns json(user)
- * @access Private
+ * @api {get} /users/my-profile myProfile
+ * @apiName myProfile
+ * @apiGroup users
+ * @apiDescription Permet d'afficher le profil de l'utilisateur connecte
+ * 
+ * @apiParam {String} id Id de l'utilisateur
+ * 
+ * @apiSuccess {object} user Profil de l'utilisateur
+ * @apiSuccess {String} user.firstname Prenom de l'utilisateur
+ * @apiSuccess {String} user.lastname Nom de l'utilisateur
+ * @apiSuccess {String} user.avatar Avatar de l'utilisateur
+ * @apiSuccess {String} user.loveStatus Situation amoureuse de l'utilisateur
+ * @apiSuccess {Number} user.zipCode Code postal de l'utilisateur
+ * @apiSuccess {String} user.city Ville de l'utilisateur
+ * @apiSuccess {String} user.bio Description de l'utilisateur
  */
 
 exports.myProfile = async function(req, res) {
@@ -150,12 +186,24 @@ exports.myProfile = async function(req, res) {
 }
 
 /**
- * Get profile by ID
+ * @api {get} /users/profile/:id profile
+ * @apiName profile
+ * @apiGroup users
+ * @apiDescription Permet d'afficher le profil d'un utilisateur dont l'id est passe en parametre
  * 
- * @param {*} req
- * @param {*} res
- * @returns json(user)
- * @access Private
+ * @apiParam {String} id Id de l'utilisateur dont on souhaite afficher le profil
+ * 
+ * @apiSuccess {object} user Profil de l'utilisateur
+ * @apiSuccess {String} user.avatar Avatar de l'utilisateur
+ * @apiSuccess {String} user.firstname Prenom de l'utilisateur
+ * @apiSuccess {String} user.lastname Nom de l'utilisateur
+ * @apiSuccess {Number} user.age Age de l'utilisateur
+ * @apiSuccess {String} user.bio Description de l'utilisateur
+ * @apiSuccess {String} user.loveStatus Situation amoureuse de l'utilisateur
+ * @apiSuccess {String} user.address Adresse de l'utilisateur
+ * @apiSuccess {Number} user.zipCode Code postal de l'utilisateur
+ * @apiSuccess {String} user.city Ville de l'utilisateur
+ * @apiSuccess {Number} user.toquesAvailable Nombre de toques disponibles de l'utilisateur
  */
 
  exports.profile = async function(req, res) {
@@ -168,14 +216,30 @@ exports.myProfile = async function(req, res) {
      }
  }
 
- /**
-  * Update profile
-  * 
-  * @param {*} req
-  * @param {*} res
-  * @returns json(user)
-  * @access Private
-  */
+/**
+ * @api {get} /users/:id updateProfile
+ * @apiName updateProfile
+ * @apiGroup users
+ * @apiDescription Permet de mettre a jour son propre profil
+ * 
+ * @apiParam {String} id Id de l'utilisateur dont on souhaite modifier le profil
+ * 
+ * @apiSuccess {object} user Profil de l'utilisateur mis a jour
+ * @apiSuccess {String} user.firstname Prenom de l'utilisateur
+ * @apiSuccess {String} user.lastname Nom de l'utilisateur
+ * @apiSuccess {Number} user.age Age de l'utilisateur
+ * @apiSuccess {String} user.email Email de l'utilisateur
+ * @apiSuccess {String} user.password Mot de passe de l'utilisateur
+ * @apiSuccess {String} user.avatar Avatar de l'utilisateur
+ * @apiSuccess {String} user.bio Description de l'utilisateur
+ * @apiSuccess {String} user.loveStatus Situation amoureuse de l'utilisateur
+ * @apiSuccess {String} user.address Adresse de l'utilisateur
+ * @apiSuccess {Number} user.zipCode Code postal de l'utilisateur
+ * @apiSuccess {String} user.city Ville de l'utilisateur
+ * @apiSuccess {Number} user.toquesAvailable Nombre de toques disponibles de l'utilisateur
+ * 
+ * @apiError EmailDejaUtilise L'email entre est deja utilise
+ */
 
 exports.updateProfile = async function(req, res) {
     const {
@@ -236,11 +300,12 @@ exports.updateProfile = async function(req, res) {
 }
 
 /**
- * Get a list of the users
- *
- * @param {*} req
- * @param {*} res
- * @access Private
+ * @api {put} /users/ listUsers
+ * @apiName listUsers
+ * @apiGroup users
+ * @apiDescription Affichage des profils des utilisateurs inscrits
+ * 
+ * @apiSuccess {object} users Liste des utilisateurs inscrits
  */
 
 exports.listUsers = async function(req, res) {
@@ -254,13 +319,20 @@ exports.listUsers = async function(req, res) {
 }
 
 /**
-  * Delete user by ID
-  * 
-  * @param {*} req
-  * @param {*} res
-  * 
-  * @desc Delete user by ID
-  * @access Private
+ * @api {delete} /users/:id deleteUser
+ * @apiName deleteUser
+ * @apiGroup users
+ * @apiDescription Suppression de l'utilisateur connecte
+ * 
+ * @apiParam {String} id Id de l'utilisateur a supprimer
+ * 
+ * @apiSuccessExample {json} Success-Response
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "msg" : "Utilisateur supprime"
+ *      }
+ *
+ * @apiError UtilisateurNonTrouve L'utilisateur dont l'id est passe en parametre n'a pas ete trouve
  */
 
 exports.deleteUser = async function(req, res) {
